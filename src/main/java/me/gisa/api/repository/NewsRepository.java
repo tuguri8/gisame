0c48b8b2d1fd71d644bcaf3bc8f6693dfcbb8fdc
ghkvud2 <ghkvud2@naver.com>
2019-08-01 19:33:04 +0900

    queryDSL 패키지 삭제

--- src/main/java/me/gisa/api/repository/NewsRepository.java
+++ src/main/java/me/gisa/api/repository/NewsRepository.java
@@ -1,21 +1,14 @@
 package me.gisa.api.repository;
 
-import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.Predicate;
-import me.gisa.api.controller.model.PageVO;
 import me.gisa.api.repository.entity.KeywordType;
 import me.gisa.api.repository.entity.News;
 import me.gisa.api.repository.entity.NewsType;
-import me.gisa.api.repository.entity.QNews;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @Repository
